81bee1a3d62d33860fff92576a3246102b6dc4e7
Pradeep kumar <115003197+vitianpradeep@users.noreply.github.com>
2023-03-14 00:19:29 +0530

    Update Day24.py

--- Day24.py
+++ Day24.py
@@ -10,34 +10,20 @@ import sys
 #
 # The function is expected to return a STRING.
 # The function accepts STRING s as parameter.
-#
+
 def timeConversion(s):
-    # Write your code here
-    s1=0
-    if s[-2:]=='AM':
+    s1 = 0
+    if s[-2:] == 'AM':
         s1 = int(s[0:2])
-        if s1==12:
-            s1 = str("00")
-            final1 = s.replace(s[0:2],s1)
-            final1 = final1.replace(s[-2:],"")
-        elif s1<12:
-            s1=str(abs(s1-12))
-        final1 = s.replace(s[0:2],s1)
-        final1 = final1.replace(s[-2:],"")
+        if s1 == 12:
+            s1 = 0
+        final1 = "{:02d}".format(s1) + s[2:-2]
         print(final1)
-        
-    elif s[-2:]=='PM':
-        s2=0
+    elif s[-2:] == 'PM':
         s2 = int(s[0:2])
-        if s2==12:
-            s2 = str("00")
-            final2 = s.replace(s[0:2],s2)
-            final2 = final2.replace(s[-2:],"")
-        elif s2<12:
+        if s2 != 12:
             s2 = s2 + 12
-            s2=str(abs(s2))
-        final2 = s.replace(s[0:2],s2)
-        final2 = final2.replace(s[-2:],"")
+        final2 = "{:02d}".format(s2) + s[2:-2]
         print(final2)
 
 if __name__ == '__main__':
