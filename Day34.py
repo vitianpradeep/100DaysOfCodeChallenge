6335129fc192de173f0e0d10c5d844cf42d57e9e
Pradeep kumar <115003197+vitianpradeep@users.noreply.github.com>
2023-03-28 21:53:57 +0530

    Add files via upload

---
+++ Day34.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    typecount = [0 for i in range(5)]
+    for i in arr:
+        typecount[i-1] += 1
+    max_count = max(typecount)
+    for i in range(5):
+        if typecount[i] == max_count:
+            return i+1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
